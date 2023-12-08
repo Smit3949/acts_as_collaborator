fc92788e8d709d839d176e0a04c188236c9efc4d
Smit Patel <smit@shipmnts.com>
2023-12-08 18:55:46 +0530

    addded default scope

--- lib/acts_as_collaborator.rb
+++ lib/acts_as_collaborator.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
 require_relative "acts_as_collaborator/version"
+require 'active_record'
 
 module ActsAsCollaborator
-  def acts_as_collaborator
-    p " you are in acts as collaborator"
+  def acts_as_collaborator(options = {})
+    class_attribute :collaborator_configuration
+
+    collaborator_configuration = {
+      column: 'collaborators',
+      column_type: 'array',
+      table_name: self.table_name
+    }
+
+    collaborator_configuration.merge!(options) 
+
+
+    unless %w[array string].include? collaborator_configuration[:column_type]
+      raise ArgumentError, "'array' or 'string' expected" \
+        " for :column_type option, got #{collaborator_configuration[:column_type]}"
+    end
+
+    default_scope lambda {
+      if ActsAsTenant.current_tenant && ::Network::UserAccount.current
+        where("#{::Network::UserAccount.current.user_contact.id} = ANY(#{collaborator_configuration[:table_name]}.#{collaborator_configuration[:column]})")
+      end
+    }
   end
 end
